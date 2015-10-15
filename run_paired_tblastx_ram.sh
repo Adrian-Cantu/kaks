c7edbc53e5a45ca9e0fd7f71f069a646adfd721e
Adrian Cantu <garbanyo@gmail.com>
2015-10-15 15:36:00 -0700

    Initial commit

---
+++ run_paired_tblastx_ram.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for q in $( ls phage_fa ); do
+	for s in $( ls fastas_filtrados ); do
+	echo "tblastx -query phage_fa/$q -subject fastas_filtrados/$s -outfmt '6 qseqid sseqid qseq qstart qend qframe sseq sstart send sframe qlen slen length evalue' -evalue 0.01 > result_tblastx/phage_tail_protein_fig""_vs_Timbebas2012_out.tblastx"
+    done
+done
