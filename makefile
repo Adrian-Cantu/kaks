15c2c78cf295a6dff6a65e807df192677c132ca0
Adrian Cantu <garbanyo@gmail.com>
2015-10-21 18:21:44 -0700

    cambie cosas en run paired para corregir errores, aun tengo problemas escapando '

--- makefile
+++ makefile
@@ -6,5 +6,8 @@ all:
 test:
 	qsub -cwd -e sge -o sge -t 1-3 ./tblastx_qsub.sh
 
+test2:
+	qsub -q important -cwd  -e sge -o sge ./tblastx_qsub.tmp
+
 clean:
 	rm sge/*
--- run_paired_tblastx_ram.sh
+++ run_paired_tblastx_ram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for q in $( ls phage_fa ); do
 	for s in $( ls fasta_filter ); do
-	echo "tblastx -query phage_fa/$q -subject fastas_filtrados/$s -outfmt '6 qseqid sseqid qseq qstart qend qframe sseq sstart send sframe qlen slen length evalue' -evalue 0.01 -out result_tblastx/phage_tail_protein_fig""_vs_Timbebas2012_out.tblastx"
+	echo "tblastx -query phage_fa/$q -subject fasta_filter/$s -outfmt '6 qseqid sseqid qseq qstart qend qframe sseq sstart send sframe qlen slen length evalue' -evalue 0.01 -out result_tblastx/$q"_"vs_$s"".tblastx"
     done
 done
